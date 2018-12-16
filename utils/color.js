9918605101ba2d8fb967cc83dd10ce3aa6da2ce1
边宇琨 <bianyukun1213@outlook.com>
2018-12-16 13:53:50 +0800

    尝试修复颜色设置问题

--- utils/color.js
+++ utils/color.js
@@ -1,4 +1,4 @@
-function apl() { //我操你见过这样的bug吗？名字起四个字母以内的就行，四个字母以外的一旦脱离开发环境，就报不是方法的错误！
+function apl() {
   var color = getApp().globalData['launchInf']['mainColor'] //这句实际上必须写里面，不然获取到的会是设置color之前的null。
   var reg = new RegExp('^#[0-9a-fA-F]{6}$')
   if (reg.test(color)) {
