bdc3af1e6f8981ae592375e29303b3f2c7761ef5
边宇琨 <bianyukun1213@outlook.com>
2020-11-12 00:01:04 +0800

    调整界面文本

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ App({
   towxml: require('/towxml/index'),
   globalData: {
     dbgMode: false,
-    version: '1.51.7',
+    version: '1.51.8',
     platform: {
       'tag': 'WeChat',
       'str': '微信'
